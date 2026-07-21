b300bdc640aaa2ab1d477b5990eebd821e0933c3
LankyAngel <angelescalante1908@gmail.com>
2026-07-20 19:33:57 -0700

    cors

--- backend/main.py
+++ backend/main.py
@@ -62,10 +62,8 @@ app.mount(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
         "https://frontend-one-jet-55.vercel.app",
-        "https://frontend-git-main-proyectocontrolescolar.vercel.app",
-        "https://frontend-8849s0yyr-proyectocontrolescolar.vercel.app",
+        "https://frontend-proyectocontrolescolar.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
